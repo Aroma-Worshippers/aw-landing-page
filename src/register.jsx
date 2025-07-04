3dbda1b18b79920a438c35a7e94c004875dc29d8
Ifechukwu Edet <ifechiglory@gmail.com>
2025-07-04 09:23:08 +0100

    bug fixes: routing issues

--- src/register.jsx
+++ src/register.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StaticHeader from "./components/StaticHeader.jsx";
-import Footer from "./components/Footer.jsx";
+import StaticFooter from "./components/StaticFooter.jsx";
 import RegistrationPage from "./pages/RegisterPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <StaticHeader />
     <RegistrationPage />
-    <Footer />
+    <StaticFooter />
   </React.StrictMode>
 );
