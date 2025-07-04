e8bb426b38433dfa1327c1f887263e64aaa9a8b4
Ifechukwu Edet <ifechiglory@gmail.com>
2025-07-04 10:37:33 +0100

    bug fixes: display error or success message in registration page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,7 +4,8 @@ import { registerUser } from "../services/api";
 export default function RegistrationPage() {
   const [errors, setErrors] = useState({});
   const [submitted, setSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); 
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -58,14 +59,27 @@ export default function RegistrationPage() {
             firstTimer: "",
           });
           setTimeout(() => setSubmitted(false), 5000);
+          setErrorMessage("");
         } else {
-          alert("Registration failed. Please try again.");
-        } 
+          setErrorMessage("Registration failed. Please try again.");
+        }
         setLoading(false);
       },
       (error) => {
         console.error("Registration failed:", error);
-        alert("Something went wrong. Please try again.");
+        const serverMsg =
+          error.response?.data?.message ||
+          "Something went wrong. Please try again.";
+        setErrorMessage(serverMsg);
+        setTimeout(() => setErrorMessage(""), 5000);
+        setFormData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phoneNumber: "",
+          church: "",
+          firstTimer: "",
+        });
         setLoading(false);
       }
     );
@@ -84,6 +98,11 @@ export default function RegistrationPage() {
         {submitted && (
           <div className="px-4 py-3 mb-6 text-green-700 bg-green-100 border border-green-400 rounded">
             Registration successful
+          </div>
+        )}
+        {errorMessage && (
+          <div className="px-4 py-3 mb-6 text-red-700 bg-red-100 border border-red-400 rounded">
+            {errorMessage}
           </div>
         )}
         <form onSubmit={handleSubmit} className="p-4">
