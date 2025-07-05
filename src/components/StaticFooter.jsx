04b2dcb5dea45e3ec8b9604165464d94034fac68
Ifechukwu Edet <ifechiglory@gmail.com>
2025-07-05 03:42:58 +0100

    bug fixes: fix external links

--- src/components/StaticFooter.jsx
+++ src/components/StaticFooter.jsx
@@ -1,13 +1,16 @@
-import { BsFacebook } from "react-icons/bs";
+import React from "react";
+import { BsFacebook, BsWhatsapp } from "react-icons/bs";
 import {
+  LuFacebook,
   LuInstagram,
   LuMail,
+  LuMap,
   LuMapPin,
   LuPhone,
   LuYoutube,
 } from "react-icons/lu";
 
-export default function StaticFooter() {
+export default function Footer() {
   return (
     <>
       <footer id="contact" className="p-8 mt-10 text-white bg-green-950">
@@ -20,13 +23,17 @@ export default function StaticFooter() {
               </p>
             </div>
             <div>
-              <img src="/assets/AW LOGO 2b 2.png" alt="Logo" className="text-sm"></img>
+              <img
+                src="/assets/AW LOGO 2b 2.png"
+                alt="Logo"
+                className="text-sm"
+              ></img>
             </div>
           </section>
           <section className="grid grid-cols-1 gap-8 mt-8 text-xl md:grid-cols-2">
             <div>
               <div className="flex py-2">
-                <LuMail/>
+                <LuMail />
                 <p className="ml-2 hover:underline">
                   <a href="mailto:aromaworshippers@gmail.com">
                     aromaworshippers@gmail.com
@@ -34,54 +41,67 @@ export default function StaticFooter() {
                 </p>
               </div>
               <div className="flex py-2">
-                <LuPhone/>
-                <p className="ml-2">
-                  +234 706 846 9754, +234 706 793 3625
-                </p>
+                <LuPhone />
+                <p className="ml-2">+234 706 846 9754, +234 706 793 3625</p>
               </div>
               <div className="flex py-2">
-                <LuInstagram/>
+                <LuInstagram />
                 <p className="ml-2 hover:underline">
-                  <a href="https://www.instagram.com/aromaworshippers">
+                  <a
+                    href="https://www.instagram.com/aromaworshippers?igsh=MXkxZ2tyZGltNDFlZA=="
+                    target="_blank"
+                  >
                     Aroma Worshippers Music Ministry
                   </a>
                 </p>
               </div>
               <div className="flex py-2">
-                <BsFacebook/>
+                <BsFacebook />
                 <p className="ml-2 hover:underline">
-                  <a href="https://www.facebook.com/AWMuicMinistry">
+                  <a
+                    href="https://www.facebook.com/share/1GC5cwEqzN/"
+                    target="_blank"
+                  >
                     @aromaworshippers
                   </a>
                 </p>
               </div>
               <div className="flex py-2">
-                <LuMapPin/>
+                <LuMapPin />
                 <p className="ml-2">Enugu, Nigeria</p>
               </div>
             </div>
             <div className="p-4">
               <h2 className="mb-2 text-xl font-bold">MEDIA</h2>
               <div className="flex py-2">
-                <LuYoutube/>
+                <LuYoutube />
                 <p className="ml-2 hover:underline">
-                  <a href="https://www.youtube.com/goodsoracel">
-                    www.youtube.com/@godsoracle
+                  <a
+                    href="https://youtube.com/@godsoracle?si=tYzbECHi8eCmP6Bd"
+                    target="_blank"
+                  >
+                    @godsoracle
                   </a>
                 </p>
               </div>
               <div className="flex py-2">
-                <BsFacebook/>
+                <BsFacebook />
                 <p className="ml-2 hover:underline">
-                  <a href="https://www.facebook.com/AWMuicMinistry">
+                  <a
+                    href="https://www.facebook.com/share/1GC5cwEqzN/"
+                    target="_blank"
+                  >
                     @aromaworshippers
                   </a>
                 </p>
               </div>
               <div className="flex py-2">
-                <LuInstagram/>
+                <LuInstagram />
                 <p className="ml-2 hover:underline">
-                  <a href="https://www.instagram.com/aromaworshippers">
+                  <a
+                    href="https://www.instagram.com/aromaworshippers?igsh=MXkxZ2tyZGltNDFlZA=="
+                    target="_blank"
+                  >
                     Aroma Worshippers Music Ministry
                   </a>
                 </p>
